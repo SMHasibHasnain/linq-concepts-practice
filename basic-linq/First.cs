e4fde20a3aa8e9a5dafdcd4d916b7bb2fcc3892c
Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-04-06 16:42:11 +0600

    Add LINQ methods First & Last for retrieving first or last element.

---
+++ basic-linq/First.cs
@@ -0,0 +1,49 @@
+
+List <Person> persons = new List<Person>() {
+    new("Hasib", "Programmer", 52345.75m),
+    new("Hamim", "Doctor", 52345.75m),
+    new("Tofayel", "Businessman", 52345.75m),
+    new("Habib", "Entrepreneur", 8700.25m),
+    new("Fardin", "Engineer", 645000.80m),
+    new("Ador", "Designer", 710500.40m),
+    new("Rajib", "Entrepreneur", 689320.10m),
+    new("fahim", "Scientist", 672150.60m),
+    new("Mr. X", "CEO", 950000.00m),
+    new("Mr. Y", "CEO", 932450.35m),
+    new("Mr. Z", "Businessman", 980000.75m),
+    new("Mr. P", "Investor", 910200.90m),
+    new("Mr. Q", "Entrepreneur", 745320.45m),
+    new("Mr. R", "Executive", 805430.20m),
+    new("Mr. A", "Programmer", 150000.55m),
+    new("Dennis Ritchie", "Computer Scientist", 142300.15m),
+    new("Bjarne Stroustrup", "Engineer", 168750.95m),
+    new("James Gosling", "Programmer", 155600.70m),
+    new("Guido van Rossum", "Programmer", 148900.30m),
+    new("Brendan Eich", "Programmer", 152340.85m)
+};
+
+var FirstPerson = persons.First();
+var FirstEntrepreneur = persons.First(person => person.Job == "Entrepreneur");
+var LastEngineer = persons.Last(person => person.Job == "Engineer");
+
+Console.WriteLine($"{nameof(FirstPerson)} = {FirstPerson.Name}");
+Console.WriteLine($"{nameof(FirstEntrepreneur)} = {FirstEntrepreneur.Name}");
+Console.WriteLine($"{nameof(LastEngineer)} = {LastEngineer.Name}");
+
+
+
+
+//Person Class
+class Person
+{
+    public string Name { get; set; }
+    public string Job { get; set; }
+    public decimal Salary { get; set; }
+
+    public Person(string N, string J, decimal S)
+    {
+        Name = N;
+        Job = J;
+        Salary = S;
+    }
+}
